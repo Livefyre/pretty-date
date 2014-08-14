04c2ef75de9b58a6e1c3ec52c5570cd85eafbcf8
Kit <bmartin@livefyre.com>
2014-08-14 13:20:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,22 @@ module.exports = function (date, i18n) {
         date.getFullYear()
     ].join('');
 };
+
+/**
+ * Get a pretty time.
+ * @param {Date} date
+ * @param [object] i18n
+ * @return {string}
+ */
+module.exports = function (date, i18n) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+    return [
+        hours < 13 ? hours : hours-12,
+        ':',
+        minutes < 10 ? '0' : '',
+        minutes,
+        ' ',
+        hours < 13 ? 'am' : 'pm',
+    ].join('');
+}
